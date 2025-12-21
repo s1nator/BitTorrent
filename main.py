352f7b287ef25f27ece6dafc3eafed7a9e0cf96a
Denis Brevnov <denisbrevnov2006@gmail.com>
2025-12-21 19:47:18 +0500

    add parser torrent file

---
+++ main.py
@@ -0,0 +1,11 @@
+from parsing_torrent_file import parsing_torrent_file
+
+
+
+def main():
+    print(parsing_torrent_file(
+        "/Users/denisbrevnov/Downloads/Baba.Is.You.v478f.rar.torrent"))
+
+
+if __name__ == "__main__":
+    main()
---
+++ parsing_torrent_file.py
@@ -0,0 +1,22 @@
+import bcoding
+
+
+def parsing_torrent_file(file_path :str) -> list[str] | None:
+    try:
+        list_args_for_torrent_file = []
+        with open(file_path, 'rb') as torrent_file:
+            raw_data = torrent_file.read()
+            torrent_data = bcoding.bdecode(raw_data)
+
+
+        if 'announce' in torrent_data:
+            list_args_for_torrent_file.append(torrent_data['announce'])
+
+        if 'info' in torrent_data:
+            info = torrent_data['info']
+            list_args_for_torrent_file.append(info['name'])
+
+        return list_args_for_torrent_file
+
+    except Exception as e:
+        print(f"Error : {e}")
